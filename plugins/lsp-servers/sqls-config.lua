f44746a415f6c34664f887252690f96f047abb45
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2021-07-09 10:50:41 +0100

    Add database connect for sqls

---
+++ plugins/lsp-servers/sqls-config.lua
@@ -0,0 +1,8 @@
+local connection_list = {
+	{
+		driver = "postgresql",
+		dataSourceName = "host=localhost port=5432 user=postgres password= dbname=tecky_roster sslmode=disable",
+	},
+}
+
+return connection_list
