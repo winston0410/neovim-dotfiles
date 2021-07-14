db6c68e929a2a1fefb6412a7ce14eaa5bf9d6a1c
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2021-07-14 12:20:48 +0100

    Use buf-num-nav

--- plugins/bufferline.lua
+++ plugins/bufferline.lua
@@ -9,8 +9,14 @@ local function init(use)
 				show_buffer_close_icons = false,
 				show_close_icon = false,
 			})
-            vim.api.nvim_set_keymap("n", "<c-g>c", ":edit ", { silent = false, noremap = true})
-            vim.api.nvim_set_keymap("n", "<c-g>q", "<cmd>bdelete<cr>", { silent = true, noremap = true})
+			vim.api.nvim_set_keymap("n", "<c-g>c", ":edit ", { silent = false, noremap = true })
+			vim.api.nvim_set_keymap("n", "<c-g>q", "<cmd>bdelete<cr>", { silent = true, noremap = true })
+		end,
+	})
+	use({
+		"winston0410/buf-num-nav.nvim",
+		config = function()
+			require("buf-num-nav").setup()
 		end,
 	})
 end
