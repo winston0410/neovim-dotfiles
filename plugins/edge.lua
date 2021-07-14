1316cb6f0fc0d62de9014038609fc41ea6f73bc5
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2021-07-14 17:42:31 +0100

    Move colorscheme setting into config()

--- plugins/edge.lua
+++ plugins/edge.lua
@@ -1,15 +1,11 @@
 local function init(paq)
 	paq({ "folke/tokyonight.nvim", config = function ()
-    end})
         vim.g.tokyonight_style = "night"
         vim.cmd("colorscheme tokyonight")
-	-- paq {'navarasu/onedark.nvim'}
-	-- vim.cmd 'colorscheme onedark'
+    end})
 end
 
 return { init = init }
-
---
 -- paq{'sainnhe/edge'}
 -- vim.g.edge_transparent_background = 1
 -- vim.g.edge_sign_column_background = 'none'
