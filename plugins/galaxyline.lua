0f7011a309a200f035902776314065c5728d6714
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2021-07-15 07:44:36 +0100

    Handle diagnostic

--- plugins/galaxyline.lua
+++ plugins/galaxyline.lua
@@ -7,9 +7,30 @@ local function init(paq)
 				options = { theme = "tokyonight" },
 				sections = {
 					lualine_a = { "mode" },
-					lualine_b = {},
+					lualine_b = { "branch" },
 					lualine_c = {},
-					lualine_x = {},
+					lualine_x = {
+						{
+							"diagnostics",
+							sources = { "nvim_lsp" },
+							symbols = { error = " ", warn = " ", info = " " },
+							-- color_error = colors.red,
+							-- color_warn = colors.yellow,
+							-- color_info = colors.cyan,
+						},
+						{
+                            -- Check if active LSP exist
+							function ()
+								local msg = ""
+								local clients = vim.lsp.get_active_clients()
+								if #clients < 1 then
+									msg = "年"
+                                    return msg
+								end
+                                return ""
+							end,
+						},
+					},
 					lualine_y = {},
 					lualine_z = {},
 				},
