0581711aa660315ba2c1b153870c031245292063
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2021-07-15 17:55:10 +0100

    Use neogit instead of fugitive

--- plugins/magit.lua
+++ plugins/magit.lua
@@ -1,8 +1,25 @@
 local function init(use)
+	-- use({
+	-- "tpope/vim-fugitive",
+	-- config = function()
+	-- end,
+	-- })
+
 	use({
-		-- "tpope/vim-fugitive",
-		"tpope/vim-fugitive",
+		"TimUntersberger/neogit",
+		requires = "nvim-lua/plenary.nvim",
 		config = function()
+			require("neogit").setup({
+				disable_signs = false,
+				disable_context_highlighting = false,
+				disable_commit_confirmation = true,
+				mappings = {
+					status = {
+						-- ["B"] = "BranchPopup",
+						-- ["s"] = "",
+					},
+				},
+			})
 		end,
 	})
 end
