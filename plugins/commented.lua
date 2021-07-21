69dc86af7ae63c9de2869587f3cb9d5e25bda418
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2021-07-21 22:33:44 +0100

    Make commented optional

--- plugins/commented.lua
+++ plugins/commented.lua
@@ -1,11 +1,13 @@
 local function init(paq)
-	paq{'winston0410/commented.nvim', config = function ()
-	require'commented'.setup{}
-    end}
+	paq({
+		"winston0410/commented.nvim",
+		keys = { { "n", "<Leader>c" }, { "n", "<Leader>cc" }, { "v", "<Leader>c" } },
+		config = function()
+			require("commented").setup({})
+		end,
+	})
 end
 
 return {
-	init = init
+	init = init,
 }
-
-
