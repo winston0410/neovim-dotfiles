646c6acd353e8e83559df03746356207c370b151
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2021-07-22 21:45:18 +0100

    Using hop with f

--- plugins/hop.lua
+++ plugins/hop.lua
@@ -2,24 +2,33 @@ local function init(use)
 	use({
 		"phaazon/hop.nvim",
 		-- "winston0410/hop.nvim",
-		keys = { { "n", "<Leader><Leader>" }, { "v", "<Leader><Leader>" } },
+		-- keys = { { "n", "<Leader><Leader>" }, { "v", "<Leader><Leader>" } },
+		keys = { { "n", "f" }, { "v", "f" },{ "n", "F" }, { "v", "F" } },
 		config = function()
 			require("hop").setup({})
-			vim.cmd([[ nmap <Leader><Leader> <cmd>HopChar1<cr>]])
-			vim.cmd([[ vmap <Leader><Leader> <cmd>HopChar1<cr>]])
-			vim.cmd([[ nmap <Leader>w <cmd>HopWord<cr>]])
-			vim.cmd([[ vmap <Leader>w <cmd>HopWord<cr>]])
 
 			local supported_modes = { "n", "v", "o" }
 
 			for _, mode in ipairs(supported_modes) do
+				-- vim.api.nvim_set_keymap(
+					-- mode,
+					-- "<Leader><Leader>",
+					-- "<cmd>HopChar1<cr>",
+					-- { noremap = true, silent = true }
+				-- )
 				vim.api.nvim_set_keymap(
 					mode,
-					"<Leader><Leader>",
-					"<cmd>HopChar1<cr>",
+					"f",
+					"<cmd>HopChar1AC<cr>",
 					{ noremap = true, silent = true }
 				)
-				vim.api.nvim_set_keymap(mode, "<Leader>w", "<cmd>HopWord<cr>", { noremap = true, silent = true })
+				vim.api.nvim_set_keymap(
+					mode,
+					"F",
+					"<cmd>HopChar1BC<cr>",
+					{ noremap = true, silent = true }
+				)
+				-- vim.api.nvim_set_keymap(mode, "<Leader>w", "<cmd>HopWord<cr>", { noremap = true, silent = true })
 				-- vim.api.nvim_set_keymap(mode, "t", "<cmd>HopChar1AC<cr>", { noremap = true, silent = true })
 				-- vim.api.nvim_set_keymap(mode, "T", "<cmd>HopChar1BC<cr>", { noremap = true, silent = true })
 			end
