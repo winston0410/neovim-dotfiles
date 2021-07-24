625130e2aba98797cd5c7c1a81e449cc6116b9b4
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2021-07-24 13:28:05 +0100

    Don't make commented optional, as ftplugin needs to run

--- plugins/commented.lua
+++ plugins/commented.lua
@@ -1,7 +1,6 @@
 local function init(paq)
 	paq({
 		"winston0410/commented.nvim",
-		keys = { { "n", "<Leader>c" }, { "n", "<Leader>cc" }, { "v", "<Leader>c" } },
 		config = function()
 			require("commented").setup({})
 		end,
