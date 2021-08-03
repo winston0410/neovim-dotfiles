a2fd98ee5328d3d4514c9b125a9d3f9d3b8dadc1
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2021-08-03 10:30:27 +0100

    Lazy load cmd-parser as well

--- plugins/range-highlight.lua
+++ plugins/range-highlight.lua
@@ -2,8 +2,8 @@ local function init(paq)
 	paq({
 		"winston0410/range-highlight.nvim",
 		event = "CmdlineEnter",
-        --Not sure why cannot lazy load it correctly
-		requires = { { "winston0410/cmd-parser.nvim" } },
+        after = "cmd-parser.nvim",
+		requires = { { "winston0410/cmd-parser.nvim", event = "CmdlineEnter" } },
 		config = function()
 			require("range-highlight").setup({})
 		end,
